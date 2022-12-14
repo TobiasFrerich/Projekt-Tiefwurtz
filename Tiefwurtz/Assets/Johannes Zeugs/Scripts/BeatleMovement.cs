3b040bf7f1762a1a63ce5002fb84408b54174e6b
Johannes Blank <joblank@stud.mediadesign.de>
2022-12-14 23:24:58 +0100

    beatles finished

--- Tiefwurtz/Assets/Johannes Zeugs/Scripts/BeatleMovement.cs
+++ Tiefwurtz/Assets/Johannes Zeugs/Scripts/BeatleMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Tiefwurtz
 {
-    public class EnemyMovement : MonoBehaviour
+    public class BeatleMovement : MonoBehaviour
     {
         [SerializeField] private float enemySpeed = 1f;
 
@@ -15,13 +15,26 @@ namespace Tiefwurtz
         private float leftStartX;
 
         private Rigidbody2D enemyBody;
+        private KäferMechanic käfMec;
 
-        private bool mussrechts = true;
+        private bool rechts = false;
+        private bool aufnRücken = false;
 
         void Start()
         {
+            //Random Start Richtung
+            int Dir = Random.Range(0, 2);
+            if (Dir == 1)
+            {
+                rechts = true;
+            }
+            else
+            {
+                rechts = false;
+            }
+
             enemyBody = GetComponent<Rigidbody2D>();
-            enemyBody.velocity = new Vector2(enemySpeed, enemyBody.velocity.y);
+            käfMec = GetComponent<KäferMechanic>();
             rightStartX = _rightMax.transform.position.x;
             leftStartX = _leftMax.transform.position.x;
         }
@@ -29,19 +42,26 @@ namespace Tiefwurtz
         private void FixedUpdate()
         {
             ChecktoFlipSprite();
+
+            aufnRücken = käfMec.aufnRücken;
         }
 
         void Update()
         {
+            if (aufnRücken)
+            {
+                return;
+            }
+
             if (enemyBody.position.x < leftStartX)
             {
-                mussrechts = true;
+                rechts = true;
             }
             else if (enemyBody.position.x > rightStartX)
             {
-                mussrechts = false;
+                rechts = false;
             }
-            if (mussrechts)
+            if (rechts)
             {
                 enemyBody.velocity = new Vector2(enemySpeed, enemyBody.velocity.y);
             }
@@ -54,11 +74,11 @@ namespace Tiefwurtz
         {
             if (enemyBody.velocity.x > 0)
             {
-                gameObject.transform.localScale = new Vector3(-0.2f, 0.2f, 0f);
+                gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0f);
             }
             if (enemyBody.velocity.x < 0)
             {
-                gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0f);
+                gameObject.transform.localScale = new Vector3(-0.2f, 0.2f, 0f);
             }
         }
     }
--- Tiefwurtz/Assets/Johannes Zeugs/Scripts/KäferMechanic.cs
+++ Tiefwurtz/Assets/Johannes Zeugs/Scripts/KäferMechanic.cs
@@ -6,21 +6,49 @@ namespace Tiefwurtz
 {
     public class KäferMechanic : MonoBehaviour
     {
+        [SerializeField] private float turnedTimer = 3f;
+        [SerializeField] private float turnCooldown = 5f;
+        [SerializeField] private float käferJumpForce = 20f;
         public bool aufnRücken { get; private set; }
-        private float rückenCounter;
+        private GameObject Player;
+        private Rigidbody2D playerBody;
 
+        private Rigidbody2D käferBody;
+        
+
+        private bool steht = true;
+
+        private void Start()
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+            käferBody = gameObject.GetComponent<Rigidbody2D>();
+            playerBody = Player.GetComponent<Rigidbody2D>();
+        }
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.gameObject.tag == "Player")
+            if (collision.gameObject.tag == "Player" && steht && (Player.transform.position.y > käferBody.position.y + 0.5f))
             {
-                aufnRücken = true;
-                Turn();
+                StartCoroutine(Turn());
+            }
+            if (collision.gameObject.tag == "Player" && aufnRücken && (Player.transform.position.y > käferBody.position.y + 0.5f))
+            {
+                playerBody.velocity = new Vector2(playerBody.velocity.x, käferJumpForce);
             }
         }
-
-        private void Turn()
+        private IEnumerator Turn()
         {
+            playerBody.velocity = new Vector2(playerBody.velocity.x, 7);
+            steht = false;
             gameObject.transform.Rotate(new Vector3(0, 0, 180));
+            käferBody.constraints = RigidbodyConstraints2D.FreezeAll;
+            yield return new WaitForSeconds(0.2f);
+            aufnRücken = true;
+            yield return new WaitForSeconds(turnedTimer);
+            gameObject.transform.Rotate(new Vector3(0, 0, 180));
+            käferBody.constraints = RigidbodyConstraints2D.None;
+            käferBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+            aufnRücken = false;
+            steht = true;
         }
     }
 }
