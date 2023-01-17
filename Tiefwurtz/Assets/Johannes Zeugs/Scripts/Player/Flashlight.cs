9d572519acb90c862c7aa5944a70cceb871a7413
SophieBellinger <120316435+SophieBellinger@users.noreply.github.com>
2023-01-17 11:28:41 +0100

    light changes

--- Tiefwurtz/Assets/Johannes Zeugs/Scripts/Player/Flashlight.cs
+++ Tiefwurtz/Assets/Johannes Zeugs/Scripts/Player/Flashlight.cs
@@ -5,21 +5,25 @@ namespace Tiefwurtz
 {
     public class Flashlight : MonoBehaviour
     {
-        public Light2D light;
+        public Light2D backLight;
+        public Light2D playerLight;
         private GameObject enemy;
         private CultistAttack cultAttack;
         public float maxLightHealth = 15;
-        public float lightLoss = 5f;
+        public float lightLossBack = 5f;
+        public float lightLossPlayer = 5f;
         public bool keepLight;
 
-        private float startIntensity;
+        private float startBackIntensity;
+        private float startPlayerIntensity;
         private bool refill = false;
         private float currentLight;
 
         private void Start()
         {
             enemy = GameObject.FindGameObjectWithTag("Enemy");
-            startIntensity = light.intensity;
+            startBackIntensity = backLight.intensity;
+            startPlayerIntensity = playerLight.intensity;
         }
 
         private void Update()
@@ -30,16 +34,18 @@ namespace Tiefwurtz
             if (keepLight)
                 return;
 
-            if (light.intensity > 0f)
+            if (backLight.intensity > 0f)
             {
-                light.intensity = light.intensity - (lightLoss * 0.01f);
+                backLight.intensity = backLight.intensity - (lightLossBack * 0.001f);
+
+                playerLight.intensity = playerLight.intensity - (lightLossPlayer * 0.001f);
             }
         }
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.tag == "Item")
             {
-                currentLight = (light.intensity + startIntensity);
+                currentLight = (backLight.intensity + startBackIntensity);
 
                 if (currentLight > maxLightHealth)
                 {
@@ -48,9 +54,10 @@ namespace Tiefwurtz
 
                 refill = true;
 
-                if (light.intensity < 1f)
+                if (backLight.intensity < 1f)
                 {
-                    light.intensity = 1f;
+                    backLight.intensity = 1f;
+                    playerLight.intensity = 1f;
                 }
             }
         }
@@ -58,14 +65,15 @@ namespace Tiefwurtz
         {
             if (refill == true)
             {
-                if (light.intensity < currentLight)
+                if (backLight.intensity < currentLight)
                 {
-                    if (light.intensity < maxLightHealth)
+                    if (backLight.intensity < maxLightHealth)
                     {
-                        light.intensity = light.intensity + 0.05f;
+                        backLight.intensity = backLight.intensity + 0.05f;
+                        playerLight.intensity = playerLight.intensity + 0.05f;
                     }
                 }
-                else if (light.intensity > (currentLight - 1))
+                else if (backLight.intensity > (currentLight - 1))
                 {
                     refill = false;
                 }
@@ -73,7 +81,7 @@ namespace Tiefwurtz
         }
         private void OnDeath()
         {
-            if (light.intensity < 0.03)
+            if (backLight.intensity < 0.03)
             {
                 cultAttack = enemy.GetComponent<CultistAttack>();
                 cultAttack.SetPlayerIsNotAlive();
