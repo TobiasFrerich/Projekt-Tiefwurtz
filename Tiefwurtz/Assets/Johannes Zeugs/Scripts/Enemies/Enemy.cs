c7902575f6a4f8ffb8f8aa628030795ccbe91a13
Johannes Blank <joblank@stud.mediadesign.de>
2023-01-18 10:39:31 +0100

    visual feedback when hitting enemy

--- Tiefwurtz/Assets/Johannes Zeugs/Scripts/Enemies/Enemy.cs
+++ Tiefwurtz/Assets/Johannes Zeugs/Scripts/Enemies/Enemy.cs
@@ -8,12 +8,22 @@ public class Enemy : MonoBehaviour
 
     public GameObject item;
     public Transform itemTransform;
+    public SpriteRenderer _spriteRenderer;
+    public Color _color;
     private void Update()
     {
         if (enemyHealth < 1f)
         {
             OnDeath();
         }
+    }
+
+    private IEnumerator Hurt()
+    {
+        Color SpriteColor = _spriteRenderer.color;
+        _spriteRenderer.color = _color;
+        yield return new WaitForSeconds(0.1f);
+        _spriteRenderer.color = SpriteColor;
     }
 
     private void OnDeath()
@@ -24,6 +34,10 @@ public class Enemy : MonoBehaviour
 
     public void TakeDamage(float dmg)
     {
+        if (enemyHealth > 1f)
+        {
+            StartCoroutine(Hurt());
+        }
         enemyHealth = enemyHealth - dmg;
     }
 }
