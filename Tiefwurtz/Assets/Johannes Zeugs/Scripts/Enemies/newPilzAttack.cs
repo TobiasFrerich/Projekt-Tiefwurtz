e6c5ff5a09417bee75b5fd0e073968fbd1473b9a
Johannes Blank <joblank@stud.mediadesign.de>
2023-02-14 16:14:25 +0100

    Checkpoints WIP

--- Tiefwurtz/Assets/Johannes Zeugs/Scripts/Enemies/newPilzAttack.cs
+++ Tiefwurtz/Assets/Johannes Zeugs/Scripts/Enemies/newPilzAttack.cs
@@ -17,8 +17,6 @@ namespace Tiefwurtz
 
         [SerializeField] private AudioSource AttackSound;
 
-
-
         public CinemachineVirtualCamera CinemachineVC;
         public Transform hiddenPoint;
         public Transform jumpOutPoint;
